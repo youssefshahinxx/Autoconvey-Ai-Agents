d6e2a423b8156f49329059d2c4bd01e568ef0d36
youssefshahinxx <153368287+youssefshahinxx@users.noreply.github.com>
2026-05-20 02:08:22 +0300

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'], // <-- Double check this line
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
