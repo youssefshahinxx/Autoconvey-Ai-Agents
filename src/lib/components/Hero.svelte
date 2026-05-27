2a4a79acbb7dcf8293f37811d2e299e16d30f48f
youssefshahinxx <153368287+youssefshahinxx@users.noreply.github.com>
2026-05-27 20:58:57 +0300

    updates

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,17 +3,17 @@
     <div class="absolute top-[0px] md:top-[0px] left-0 w-full h-full bg-gradient-to-t from-[#fffefa]/10 to-transparent opacity-50 pointer-events-none z-[-1]"></div>
     
     <div class="max-w-4xl py-25 md:py-18 mx-auto">
-        <div class="inline-flex items-center gap-2 border border-[#37352f] text-[#37352f] px-3 py-1 rounded-full my-4 text-sm font-medium">
+        <div class="inline-flex items-center mr-auto gap-2 border border-[#37352f] text-[#37352f] px-3 py-1 rounded-full my-4 text-sm font-medium">
         <!-- <span class="material-icons" aria-hidden="true">support_agent</span> -->
         <span>AI-Powered Business Automation</span>
     </div>
         <h1 class="text-left text-4xl md:text-6xl font-bold tracking-tight mb-6 leading-tight">
             We Build AI Agents That Work Inside Your Business
         </h1>
-        <p class="text-xl text-left text-[#787774] mb-4 max-w-2xl text-shadow-sm text-shadow-[#14141412]">
+        <p class="text-xl text-left text-[#787774] mb-4 max-w-2xl">
             Autoconvey designs and manages AI agents that handle calls, qualify leads, automate workflows, and execute real operational tasks across your business.
         </p>
-        <p class="text-md text-left text-[#a1a19f] mb-10 italic text-shadow-sm text-shadow-[#14141412]">
+        <p class="text-md text-left text-[#a1a19f] mb-10 italic">
             From voice agents to custom AI workers, we build systems that don’t just respond — they act.
         </p>
         <div class="flex flex-col md:flex-row gap-4 items-center">
